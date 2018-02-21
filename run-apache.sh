03010ec344a87ea83405396a0bf45fa951340174
Unknown <peter.gallerani@gmail.com>
2018-02-20 20:35:26 -0700

    Add easier naming

--- run-apache.sh
+++ run-apache.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+
+if [[ ${1} == "" ]]; then
+  echo 'Please pass a container name'
+  exit 1
+fi
+
 docker run \
-  -d -t \
-  --name=apache \
-  --hostname=apache \
+  -d \
+  -t \
+  --name=${1} \
+  --hostname=${1} \
   --rm \
   -p 8080:80 \
   -m 64m \
